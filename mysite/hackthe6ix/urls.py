21fb41045dc9b394eb6ea78d953bc9adb6ff7687
Sung Hoon Sim <shsim@edu.uwaterloo.ca>
2018-08-25 04:26:12 -0400

    Added login form and email verification page (yet to add actual verification backend)

--- mysite/hackthe6ix/urls.py
+++ mysite/hackthe6ix/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'hackthe6ix'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('registration/', views.registration, name="register"),
+    path('login/', views.index, name="login"),
+    path('verify/', views.verify, name="verify"),
 ]
--- mysite/hackthe6ix/views.py
+++ mysite/hackthe6ix/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):	
+	return render(request, 'hackthe6ix/login.html')
+
+def registration(request):
 	return render(request, 'hackthe6ix/registration.html')
+
+def verify(request):
+	return render(request, 'hackthe6ix/verify.html')
