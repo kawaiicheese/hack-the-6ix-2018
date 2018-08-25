3a1d2ab677e7cb2102a7403c16b6323d48a0d27d
kawaiicheese <josephlu116@gmail.com>
2018-08-25 03:27:21 -0400

    added models

--- mysite/hackthe6ix/models.py
+++ mysite/hackthe6ix/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Listings(models.Model):
+    street_name =   models.CharField(max_length=30)
+    house_number =  models.PositiveIntegerField()
+    #full_location = house_number + street_name
+    price =         models.DecimalField(max_digits=5, decimal_places=2)
+    lease_length =  models.PositiveIntegerField()
+
+class Landlords(models.Model):
+    first_name =    models.CharField(max_length=30)
+    last_name =     models.CharField(max_length=30)
+
+class Tenants(models.Model):
+    first_name =    models.CharField(max_length=30)
+    last_name =     models.CharField(max_length=30)
+    
