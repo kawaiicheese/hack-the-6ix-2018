e177ff09222ae7da062b5756975bd8b3e78d5769
gucci-ninja <beautyspell5@gmail.com>
2018-08-25 09:03:36 -0400

    map

--- mysite/hackthe6ix/urls.py
+++ mysite/hackthe6ix/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('registration/', views.registration, name="register"),
     path('login/', views.index, name="login"),
     path('verify/', views.verify, name="verify"),
-    path('map.html', views.map, name='map')
+    path('map.html', views.map, name='map'),
 ]
