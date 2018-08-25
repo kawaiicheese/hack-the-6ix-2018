1f3c1aa21350150e2eb754a5120172aede80cdd5
Sung Hoon Sim <shsim@edu.uwaterloo.ca>
2018-08-25 01:05:20 -0400

    file structure changes

--- mysite/hackthe6ix/views.py
+++ mysite/hackthe6ix/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-	return render(request, 'hackthe6ix/index.html')
+def index(request):	
+	return render(request, 'hackthe6ix/registration.html')
