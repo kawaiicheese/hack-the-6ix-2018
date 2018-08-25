5522013fa6d734b4af997e6db1da29276cdc159b
Sung Hoon Sim <shsim@edu.uwaterloo.ca>
2018-08-25 00:44:47 -0400

    added index.html and routing

--- mysite/hackthe6ix/views.py
+++ mysite/hackthe6ix/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	return render(request, 'hackthe6ix/index.html')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hackthe6ix.apps.Hackthe6IxConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
